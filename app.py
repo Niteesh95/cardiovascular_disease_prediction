65f42a1524c4a66d08b3c3035a1d56b6dfa404af
Niteesh <niteesh5595@gmail.com>
2020-07-02 21:56:04 +0530

    modification

--- app.py
+++ app.py
@@ -44,9 +44,9 @@ def predict():
         my_prediction = classifier.predict(final_features)
 
         if my_prediction:
-            return render_template('index.html', prediction="Oops! You must have CVD, consult a doctor.")
+            return render_template('index.html', prediction_text="Oops! You must have CVD, consult a doctor.")
         else:
-            return render_template('index.html', prediction="Yay! negative, no CVD.")
+            return render_template('index.html', prediction_text="Yay! negative, no CVD.")
 
     else:
         return render_template('index.html')
