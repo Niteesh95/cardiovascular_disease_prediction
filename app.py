13ebb8f060d5526f81bdef7450c9181b6f197d82
Niteesh <niteesh5595@gmail.com>
2020-07-03 15:03:41 +0530

    modification

--- app.py
+++ app.py
@@ -27,30 +27,26 @@ def predict():
     for rendering result on HTML GUI
     """
 
-    if request.method == 'POST':
-        age = int(request.form['age'])
-        height = int(request.form['height'])
-        weight = int(request.form['weight'])
-        systolic_bp = int(request.form['systolic_bp'])
-        diastolic_bp = int(request.form['diastolic_bp'])
-        cholesterol = int(request.form['cholesterol'])
-        glucose = int(request.form['glucose'])
-        smoke = int(request.form['smoke'])
-        alcoholic = int(request.form['alcoholic'])
-        active = int(request.form['active'])
-        gender = int(request.form['gender'])
+    age = int(request.form['age'])
+    height = int(request.form['height'])
+    weight = int(request.form['weight'])
+    systolic_bp = int(request.form['systolic_bp'])
+    diastolic_bp = int(request.form['diastolic_bp'])
+    cholesterol = int(request.form['cholesterol'])
+    glucose = int(request.form['glucose'])
+    smoke = int(request.form['smoke'])
+    alcoholic = int(request.form['alcoholic'])
+    active = int(request.form['active'])
+    gender = int(request.form['gender'])
 
-        final_features = [[age,gender,height,weight,systolic_bp,diastolic_bp,cholesterol,glucose,smoke,alcoholic,active]]
-        final_features = StandardScaler().fit_transform(final_features)
-        my_prediction = classifier.predict(final_features)
+    final_features = [[age,gender,height,weight,systolic_bp,diastolic_bp,cholesterol,glucose,smoke,alcoholic,active]]
+    my_prediction = classifier.predict(final_features)
 
-        if my_prediction:
-            return render_template('index.html', prediction_text="Oops! You must have CVD, consult a doctor.")
-        else:
-            return render_template('index.html', prediction_text="Yay! negative, no CVD.")
-
+    if my_prediction:
+        return render_template('index.html', prediction_text="Oops! You must have CVD, consult a doctor.")
     else:
-        return render_template('index.html')
+        return render_template('index.html', prediction_text="Yay! negative, no CVD.")
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=5000, debug=True)
