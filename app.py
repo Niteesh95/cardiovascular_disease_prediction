ef4d5c878e05fec414eaafbd89d4f558f689c4da
Niteesh <niteesh5595@gmail.com>
2020-07-03 19:37:07 +0530

    modification

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 pickle_in = open('cvd-prediction-svc-model.pkl', 'rb')
 classifier = pickle.load(pickle_in)
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def home():
     return render_template('index.html')
 
